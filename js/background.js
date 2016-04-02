e741fec1f4f7ced90839e67d570ec32ed2be569d
Me <hpatel90@illinois.edu>
2016-04-02 01:20:32 -0500

    Adding caching

--- js/background.js
+++ js/background.js
@@ -1,4 +1,5 @@
-var FEED_URL = 'http://cors.io/?u=http://news.google.com/?output=rss';
+// var FEED_URL = 'http://cors.io/?u=http://news.google.com/?output=rss';
+var FEED_URL = 'https://news.google.com/?output=rss';
 
 var settings =  {FEED_URL: FEED_URL, FEED_ITEMS_COUNT: 10, SHOW_IMAGES: true, SHOW_DESCRIPTION: true};
 
@@ -9,9 +10,8 @@ $(document).ready(function() {
 	getObject(settings,
 		function(data) {
 			settings = data;
-
 			updateSettingsItems();
-			displayFeed();
+			loadAndDisplayFeed();
 		}
 	);
 
@@ -31,12 +31,26 @@ function saveSettingsItems() {
 
 	settings = {FEED_URL: FEED_URL, FEED_ITEMS_COUNT: feedItemsCount, SHOW_IMAGES: showImages , SHOW_DESCRIPTION: showDescription};
 	clearFeed();
-	displayFeed();
+	loadAndDisplayFeed();
 
 	setObject(settings,
 		function() {
 			$('#modalLoadingText').html("Saving...");
 			$('#myModal').modal('toggle');
+		}
+	);
+}
+
+function saveToCache(feed) {
+	setLargeObject("feedCache", JSON.stringify(feed),
+		function() {}
+	);
+}
+
+function readFromCache(callback) {
+	getLargeObject("feedCache",
+		function(data) {
+			callback(JSON.parse(data));
 		}
 	);
 }
@@ -52,42 +66,53 @@ function clearFeed() {
 	$('#rss-content').empty();
 }
 
-function displayFeed () {
+function loadAndDisplayFeed () {
 	jQuery.getFeed({
 		url: FEED_URL,
 		success: function(feed) {
-			$('#rss-content').append('<h3 id="rss-title">' + feed.title + '</h3><hr>');
-
-			for (var i = 0; (i<feed.items.length) && (i<settings.FEED_ITEMS_COUNT); i++) {
-				var entry = feed.items[i];
-
-				entry.description = entry.description.replace(new RegExp('<img src="//', 'g'), '<img src="http://');
-
-				var dirtyDescription = $.parseHTML(entry.description);
-
-				var image = '';
-				if(settings.SHOW_IMAGES) {
-					image = '<img class="media-object" src="' + $(dirtyDescription).find('img').attr('src') + '" alt="Image">';
-				}
-
-				var title = entry.title;
-				var link = entry.link;
-
-				var description = '';
-				if(settings.SHOW_DESCRIPTION) {
-					description = dirtyDescription[0].childNodes[0].childNodes[0].childNodes[1].childNodes[0].childNodes[2].childNodes[4].innerHTML;
-				}
-
-				var newsItemTemplate = '<div class="media news-item"> <div class="media-left"> <a href="ITEM-LINK"> ITEM-IMAGE </a> </div> <div class="media-body"> <h5 class="media-heading"><a href="ITEM-LINK">ITEM-TITLE</a></h5><div class="item-description"> ITEM-DESCRIPTION </div></div> </div><hr>';
-
-				newsItemTemplate = newsItemTemplate.replace("ITEM-IMAGE", image);
-				newsItemTemplate = newsItemTemplate.replace("ITEM-TITLE", title);
-				newsItemTemplate = newsItemTemplate.replace("ITEM-LINK", link);
-				newsItemTemplate = newsItemTemplate.replace("ITEM-LINK", link);
-				newsItemTemplate = newsItemTemplate.replace("ITEM-DESCRIPTION", description);
-
-				$('#rss-content').append(newsItemTemplate);
+			if (feed.items.length == 0) {
+				readFromCache(function(feed) {
+					displayFeed(feed);
+				} );
+			} else {
+				saveToCache(feed);
+				displayFeed(feed);
 			}
 		}
 	});
+}
+
+function displayFeed(feed) {
+	$('#rss-content').append('<h3 id="rss-title">' + feed.title + '</h3><hr>');
+	for (var i = 0; (i<feed.items.length) && (i<settings.FEED_ITEMS_COUNT); i++) {
+
+		var entry = feed.items[i];
+
+		entry.description = entry.description.replace(new RegExp('<img src="//', 'g'), '<img src="http://');
+
+		var dirtyDescription = $.parseHTML(entry.description);
+
+		var image = '';
+		if(settings.SHOW_IMAGES) {
+			image = '<img class="media-object" src="' + $(dirtyDescription).find('img').attr('src') + '" alt="Image">';
+		}
+
+		var title = entry.title;
+		var link = entry.link;
+
+		var description = '';
+		if(settings.SHOW_DESCRIPTION) {
+			description = dirtyDescription[0].childNodes[0].childNodes[0].childNodes[1].childNodes[0].childNodes[2].childNodes[4].innerHTML;
+		}
+
+		var newsItemTemplate = '<div class="media news-item"> <div class="media-left"> <a href="ITEM-LINK"> ITEM-IMAGE </a> </div> <div class="media-body"> <h5 class="media-heading"><a href="ITEM-LINK">ITEM-TITLE</a></h5><div class="item-description"> ITEM-DESCRIPTION </div></div> </div><hr>';
+
+		newsItemTemplate = newsItemTemplate.replace("ITEM-IMAGE", image);
+		newsItemTemplate = newsItemTemplate.replace("ITEM-TITLE", title);
+		newsItemTemplate = newsItemTemplate.replace("ITEM-LINK", link);
+		newsItemTemplate = newsItemTemplate.replace("ITEM-LINK", link);
+		newsItemTemplate = newsItemTemplate.replace("ITEM-DESCRIPTION", description);
+
+		$('#rss-content').append(newsItemTemplate);
+	}
 }
