51f6286947ed31ad426f3a5a1979ff59d2227fbc
Me <hpatel90@illinois.edu>
2016-12-17 16:05:25 -0600

    Add theme images and selectable themes

--- js/background.js
+++ js/background.js
@@ -1,7 +1,10 @@
 // var FEED_URL = 'http://cors.io/?u=http://news.google.com/?output=rss';
 var FEED_URL = 'https://news.google.com/?output=rss';
 
-var settings =  {FEED_URL: FEED_URL, FEED_ITEMS_COUNT: 10, SHOW_IMAGES: true, SHOW_DESCRIPTION: true};
+var settings =  {FEED_URL: FEED_URL, FEED_ITEMS_COUNT: 10, THEME: 'ocean', SHOW_IMAGES: true, SHOW_DESCRIPTION: true};
+
+var IMAGE_DIRECTORY = '../images/themes/';
+var THEME_IMAGES = {ocean: 'ocean.jpg', brick: 'brick.jpg', farm: 'farm.jpg', mountain: 'mountain.jpg', beach: 'beach.jpg', beach2: 'beach2.jpg', waterfall: 'waterfall.jpg'};
 
 $(document).ready(function() {
 	var _appendElement = '<input type="hidden" name="cx" value="013056768840040662406:xjc9ebnmjlk">';
@@ -11,6 +14,7 @@ $(document).ready(function() {
 		function(data) {
 			settings = data;
 			updateSettingsItems();
+			setTheme();
 			loadAndDisplayFeed();
 		}
 	);
@@ -25,12 +29,14 @@ $(document).ready(function() {
 function saveSettingsItems() {
 	var feedURL = $('#feedURL').val();
 	var feedItemsCount = $('#feedItemsCount').val();
+	var theme = $('#theme').val();
 	var showImages = $('#showImages').is(':checked');
 	var showDescription = $('#showDescription').is(':checked');
 
-	settings = {FEED_URL: feedURL, FEED_ITEMS_COUNT: feedItemsCount, SHOW_IMAGES: showImages , SHOW_DESCRIPTION: showDescription};
+	settings = {FEED_URL: feedURL, FEED_ITEMS_COUNT: feedItemsCount, THEME: theme, SHOW_IMAGES: showImages , SHOW_DESCRIPTION: showDescription};
 	clearFeed();
 	loadAndDisplayFeed();
+	setTheme();
 
 	setObject(settings,
 		function() {
@@ -54,8 +60,15 @@ function readFromCache(callback) {
 	);
 }
 
+function setTheme() {
+	var themeImage = IMAGE_DIRECTORY + THEME_IMAGES[settings.THEME];
+	$('#header').css('background-image', 'url(' + themeImage + ')');
+	console.log(themeImage);
+}
+
 function updateSettingsItems() {
 	$('#feedURL').val(settings.FEED_URL);
+	$('#theme').val(settings.THEME);
 	$('#feedItemsCount').val(settings.FEED_ITEMS_COUNT);
 	$('#showImages').prop('checked', settings.SHOW_IMAGES);
 	$('#showDescription').prop('checked', settings.SHOW_DESCRIPTION);
@@ -105,7 +118,6 @@ function displayFeed(feed) {
 		}
 
 		var newsItemTemplate = '<div class="media news-item"> <div class="media-left"> <a href="ITEM-LINK"> ITEM-IMAGE </a> </div> <div class="media-body"> <h5 class="media-heading"><a href="ITEM-LINK">ITEM-TITLE</a></h5><div class="item-description"> ITEM-DESCRIPTION </div></div> </div><hr>';
-
 		newsItemTemplate = newsItemTemplate.replace("ITEM-IMAGE", image);
 		newsItemTemplate = newsItemTemplate.replace("ITEM-TITLE", title);
 		newsItemTemplate = newsItemTemplate.replace("ITEM-LINK", link);
