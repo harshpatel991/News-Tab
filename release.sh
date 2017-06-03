57a9d47cd154acbd7b88758b51d77f371dfd0002
harshpatel991 <hpatel90@illinois.edu>
2017-06-03 02:27:15 -0500

    Add release versions to index.html and background.js

--- release.sh
+++ release.sh
@@ -2,12 +2,23 @@
 
 printf "${RED}------------Building Release------------${NC}\n"
 
-echo "---Minifying JS and CSS"
-gulp
+DATE=`date +%Y-%m-%d`
 
 lastVersion=$(git tag | tail -n1)
 echo "---Tag version [last was ${lastVersion}]"
 read tag
+
+#Update the app version in the index.html file and background.js file
+sed -i .bak "s/\<\!-- app-version --\>.*\<\!-- app-version --\>/\<\!-- app-version --\>${tag}\<\!-- app-version --\>/g" index.html
+sed -i .bak "s/\<\!-- app-date --\>.*\<\!-- app-date --\>/\<\!-- app-date --\>${DATE}\<\!-- app-date --\>/g" index.html
+sed -i .bak "s;/\*app-version\*/.*/\*app-version\*/;/\*app-version\*/\"${tag}\"/\*app-version\*/;g" js/background.js
+
+#Remove the backup files created by sed
+rm index.html.bak
+rm js/background.js.bak
+
+echo "---Minifying JS and CSS"
+gulp
 
 sed "s/\"version.*/\"version\": \"${tag}\",/" manifest.json > manifest.tmp;
 mv manifest.tmp manifest.json
